adf5606b5632f9936342ce387ed4f5f439d460ca
krl8456 <karolrak50@gmail.com>
2022-04-03 02:08:27 +0200

    Add picture and informations

--- index.js
+++ index.js
@@ -2,13 +2,41 @@ const body = document.querySelector('body');
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const closeIcon = document.querySelector('.close-icon');
 const hamburgerIcon = document.querySelector('.hamburger');
+const images = ["images/image-product-1.jpg", "images/image-product-2.jpg", "images/image-product-3.jpg", "images/image-product-4.jpg"];
+const previousImageButton = document.querySelector('.circle1');
+const nextImageButton = document.querySelector('.circle2');
+const pictureOfShoes = document.querySelector('.shoe');
+const amountCounter = document.querySelector('.amount-counter');
+const minusSign = document.querySelector('.minus');
+const plusSign = document.querySelector('.plus');
+
+let counter = 0;
+let amountOfPurchases = 0;
 closeIcon.addEventListener('click', () => {
     hamburgerMenu.close();
-    // hamburgerMenu.classList.remove('open-menu');
-    // hamburgerMenu.classList.add('close-menu');
 });
 hamburgerIcon.addEventListener('click', () => {
     hamburgerMenu.showModal();
-    // hamburgerMenu.classList.remove('close-menu');
-    // hamburgerMenu.classList.add('open-menu');
 });
+nextImageButton.addEventListener('click', () => {
+    if (counter === 3) {
+        counter = -1;
+    }
+    pictureOfShoes.src = images[++counter];
+});
+previousImageButton.addEventListener('click', () => {
+    if (counter === 0) {
+        counter = 4;
+    }
+    pictureOfShoes.src = images[--counter];
+});
+minusSign.addEventListener('click', () => {
+    if (amountOfPurchases > 0) {
+        amountOfPurchases --;
+    }
+    amountCounter.textContent = amountOfPurchases;
+});
+plusSign.addEventListener('click', () => {
+    amountOfPurchases++;
+    amountCounter.textContent = amountOfPurchases;
+})
